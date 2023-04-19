0420c3269d732e12ca61a094ef3153eaa813b4b6
volkanili <volkanili@yahoo.com>
2023-04-19 07:42:08 +0300

    Add support for command-line arguments

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import sys
+
+
 def greet(name, greeting="Hello"):
     return f"{greeting}, {name}!"
 
@@ -7,7 +10,12 @@ def get_input(prompt):
 
 
 if __name__ == "__main__":
-    user_name = get_input("Enter your name: ")
-    user_greeting = get_input("Enter a greeting (leave empty for default): ")
+    if len(sys.argv) > 1:
+        user_name = sys.argv[1]
+        user_greeting = sys.argv[2] if len(sys.argv) > 2 else ""
+    else:
+        user_name = get_input("Enter your name: ")
+        user_greeting = get_input("Enter a greeting (leave empty for default): ")
+
     greeting = greet(user_name, user_greeting) if user_greeting else greet(user_name)
     print(greeting)
