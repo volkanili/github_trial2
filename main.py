30acdc6edbab82c26b29bfe8cad098c690f72217
volkanili <volkanili@yahoo.com>
2023-04-19 07:41:09 +0300

    Refactor input logic into get_input function

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ def greet(name, greeting="Hello"):
     return f"{greeting}, {name}!"
 
 
+def get_input(prompt):
+    return input(prompt)
+
+
 if __name__ == "__main__":
-    user_name = input("Enter your name: ")
-    user_greeting = input("Enter a greeting (leave empty for default): ")
+    user_name = get_input("Enter your name: ")
+    user_greeting = get_input("Enter a greeting (leave empty for default): ")
     greeting = greet(user_name, user_greeting) if user_greeting else greet(user_name)
     print(greeting)
