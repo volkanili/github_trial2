00227a2db8300c568aa9ccb3a202d48c6e76c41b
volkanili <volkanili@yahoo.com>
2023-04-19 07:11:41 +0300

    Add support for custom greetings

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-def greet(name):
-    return f"Hello, {name}!"
+def greet(name, greeting="Hello"):
+    return f"{greeting}, {name}!"
 
 
 if __name__ == "__main__":
     user_name = input("Enter your name: ")
-    greeting = greet(user_name)
+    user_greeting = input("Enter a greeting (leave empty for default): ")
+    greeting = greet(user_name, user_greeting) if user_greeting else greet(user_name)
     print(greeting)
