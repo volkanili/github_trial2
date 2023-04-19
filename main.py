b9626ea82171fca60d3d15c5e3fd875aad130e17
volkanili <volkanili@yahoo.com>
2023-04-19 08:08:59 +0300

    Revert "Refactor input logic into get_input function"
    This reverts commit 30acdc6e

--- main.py
+++ main.py
@@ -1,25 +1,9 @@
-import sys
-
-
 def greet(name, greeting="Hello"):
     return f"{greeting}, {name}!"
 
 
-def get_input(prompt):
-    return input(prompt)
-
-
-def main():
-    if len(sys.argv) > 1:
-        user_name = sys.argv[1]
-        user_greeting = sys.argv[2] if len(sys.argv) > 2 else ""
-    else:
-        user_name = get_input("Enter your name: ")
-        user_greeting = get_input("Enter a greeting (leave empty for default): ")
-
+if __name__ == "__main__":
+    user_name = input("Enter your name: ")
+    user_greeting = input("Enter a greeting (leave empty for default): ")
     greeting = greet(user_name, user_greeting) if user_greeting else greet(user_name)
     print(greeting)
-
-
-if __name__ == "__main__":
-    main()
